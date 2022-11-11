523adac87c3ea23c58afb5bcb0e66599a8e3187d
Tim Kindregan <timothyj.kindregan@gmail.com>
2022-11-10 19:14:03 -0500

    commit tree c11d5e5407ead5a1d7ba4a0ece8dbf04885ada0a

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if (value <= 400) {
+    return 'This one is on me!';
+  }
+  else if (value >= 400 && value <= 2000) {
+    return 'That will be twenty bucks.';
+  }
 }
 
 function ternaryCheckCity(){
